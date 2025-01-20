8809ecd0beb49cb59f874b37aaa84d7c4937d5bd
KirkN <minhkhoi.nguyenle@gmail.com>
2025-01-20 23:02:37 +0100

    small syntax edits

--- extra_scripts/SuspGraphGenerator.py
+++ extra_scripts/SuspGraphGenerator.py
@@ -9,15 +9,11 @@ leverArm_y = 0.275
 bmp1 = []
 cmb1 = []
 toe1 = []
-snt = []
-snt2 = []
 #enter arrays
 for i in range(280):
     bmp1.append(bmp[i]*leverArm_y*100)
     cmb1.append(np.degrees(cmb[i]))
-    snt.append(np.degrees(cmb[i]))
     toe1.append(np.degrees(toe[i]))
-    snt2.append(np.degrees(toe[i]))
 
 o_cmb = 32000*(2*np.pi)
 o_toe = 300*2*np.pi
@@ -28,8 +24,9 @@ pp_toe = fl.postProcess(filtered_toe, 8)
 
 #ec_cmb = fl.edgeComp(bmp1, filtered_cmb, pp_cmb, o_cmb, 0.000005, 2)
 #ec_toe = fl.edgeComp(bmp1, filtered_toe, pp_toe, o_toe, 0.0005, 2)
+
+
 figure, ax1 = subplots()
-#ax1.plot(bmp1,cmb1, label = 'Raw Data', color = 'red')
 ax1.plot(bmp1[0: len(bmp1)-2],pp_cmb, label = 'Filter + PP', color = 'blue')
 figure.supxlabel('Bump Travel (cm)')
 figure.supylabel('Camber (Degrees)')
@@ -37,9 +34,7 @@ figure.legend()
 tight_layout()
 
 figure, ax2 = subplots(1)
-#ax2.plot(toe1,bmp1, label = 'Raw Data', color = 'red')
 ax2.plot(pp_toe,bmp1[0: len(bmp1)-8], label = 'Filter + PP', color = 'blue')
-#ax2.plot(bmp1,ec_toe, color = 'red')
 figure.supxlabel('Toe (Degrees)')
 figure.supylabel('Bump Travel (cm)')
 ax2.spines['bottom'].set_position('center')
