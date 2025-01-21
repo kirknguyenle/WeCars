b6037ebe2a538a579f33679d4e4ca8d8476af075
KirkN <minhkhoi.nguyenle@gmail.com>
2025-01-21 01:27:07 +0100

    help

--- controllers/CarBuilder/CarBuilder.py
+++ controllers/CarBuilder/CarBuilder.py
@@ -5,22 +5,39 @@ import os.path
 import math 
 import json
 
+#Instantiate Supervisor
+robot = Supervisor()
+car_node = robot.getFromDef('car')
+children_field = car_node.getField('children')
 
 
 #Read Trackwidth, WheelBase
 #Read SuspensionData 
+with open('C:\Users\minhk\OneDrive\Desktop\DriveLab\WeCars\car_files\mr2.json', 'r') as f:
+    data = json.load(f)
 
 #Calculate "SingleSuspension" Width 
 
+corner_width = data["wheel_anchor"][1]+data["tire_width"]
+print(corner_width)
 #Calculate "SingleSuspension" X Offset
+x_offset = data["wheel_anchor"][0]
+print(x_offset)
+#Generate Min-Box (Box with mass of simulated car, allowing for 4 wheels to place wheels at edges)
+
+length = data["wheel_base"]-2*x_offset
+width = data["track_width"]-2*corner_width
+height = 2/3
+
+children_field.importMFNodeFromString(0, 'car_body { dimensions ',str(length),' ',str(width),' ',str(height),' car_mass', str(data["car_mass"]),' }')
+
+
 
 #Generate Contact Properties
 
-#Generate Min-Box (Box with mass of simulated car, allowing for 4 wheels to place wheels at edges)
-
-#Place IMU in Min-Box
-
 #Generate Front Left and place
+
+children_field.importMFNodeFromString(0, 'SingleSuspension' )
 
 #Generate Front Right, Flipping Y cords of suspension, then place
 
--- extra_scripts/SuspGraphGenerator.py
+++ extra_scripts/SuspGraphGenerator.py
@@ -2,7 +2,7 @@ import pathlib
 from numpy import *
 from matplotlib.pyplot import *
 import FirstOrderFilter as fl
-bmp,cmb,cst,toe,strA,stR = loadtxt(r"C:\Users\minhk\OneDrive\Desktop\DriveLab\WeCars\testData\SuspGraph20-01-2025-20-29-34.txt", delimiter=',', unpack=True)
+bmp,cmb,cst,toe,strA,stR = loadtxt(r"C:\Users\minhk\OneDrive\Desktop\DriveLab\WeCars\test_data\SuspGraph20-01-2025-22-59-49.txt", delimiter=',', unpack=True)
 
 leverArm_y = 0.275
 #create arrays for bump steer
@@ -15,9 +15,9 @@ for i in range(280):
     cmb1.append(np.degrees(cmb[i]))
     toe1.append(np.degrees(toe[i]))
 
-o_cmb = 32000*(2*np.pi)
+o_cmb = 1600*(2*np.pi)
 o_toe = 300*2*np.pi
-filtered_cmb = fl.firstOrderFilter(bmp1, cmb1, o_cmb, 0.000005)
+filtered_cmb = fl.firstOrderFilter(bmp1, cmb1, o_cmb, 0.0000005)
 filtered_toe = fl.firstOrderFilter(bmp1, toe1, o_toe, 0.0005)
 pp_cmb = fl.postProcess(filtered_cmb, 2)
 pp_toe = fl.postProcess(filtered_toe, 8)
@@ -27,6 +27,7 @@ pp_toe = fl.postProcess(filtered_toe, 8)
 
 
 figure, ax1 = subplots()
+ax1.plot(bmp1, cmb1, label = 'sanity check', color = 'orange')
 ax1.plot(bmp1[0: len(bmp1)-2],pp_cmb, label = 'Filter + PP', color = 'blue')
 figure.supxlabel('Bump Travel (cm)')
 figure.supylabel('Camber (Degrees)')
